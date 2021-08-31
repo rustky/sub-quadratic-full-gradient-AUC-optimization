934229a3e1c2c30c6a1772b43dbb1431277357b3
rustky <50724477+rustky@users.noreply.github.com>
2021-08-31 10:29:58 -0700

    fixing prediction variable name

--- all_pairs_square_loss.py
+++ all_pairs_square_loss.py
@@ -2,7 +2,6 @@ import numpy as np
 
 def all_pairs_square_loss(predictions,labels,margin):
     a_coeff, b_coeff, c_coeff, running_loss = 0,0,0,0
-    labels_length = len(labels)
     I_pos = np.where(labels == 1)[0]
     I_neg = np.where(labels == 0)[0]
     for i in I_pos:
@@ -10,7 +9,7 @@ def all_pairs_square_loss(predictions,labels,margin):
         a_coeff += 1
         b_coeff += (-2)*z_coeff
         c_coeff += z_coeff**2
-    for j in I_neg:
-        prediction_indicies = predictions[j]
-        running_loss += a_coeff*(prediction_indicies**2) + b_coeff*prediction_indicies + c_coeff
+    for j in I_neg[0]:
+        predicted_value = predictions[j]
+        running_loss += a_coeff*(predicted_value**2) + b_coeff*predicted_value + c_coeff
     return running_loss
