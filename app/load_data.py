22282338b3757e259063533a2e3f32cbf4516911
rustky <kylerustxc@gmail.com>
2021-09-30 19:06:04 -0700

    code clean up

--- app/load_data.py
+++ app/load_data.py
@@ -41,30 +41,6 @@ class ImageDataset(Dataset):
         return image, target
 
 
-class StratifiedBatchSampler:
-    """Stratified batch sampling
-    Provides equal representation of target classes in each batch
-    """
-    def __init__(self, y, batch_size, shuffle=True):
-        if torch.is_tensor(y):
-            y = y.numpy()
-        assert len(y.shape) == 1, 'label array must be 1D'
-        n_batches = int(len(y) / batch_size)
-        self.skf = StratifiedKFold(n_splits=n_batches, shuffle=shuffle)
-        self.X = torch.randn(len(y),1).numpy()
-        self.y = y
-        self.shuffle = shuffle
-
-    def __iter__(self):
-        if self.shuffle:
-            self.skf.random_state = torch.randint(0,int(1e8),size=()).item()
-        for train_idx, test_idx in self.skf.split(self.X, self.y):
-            yield test_idx
-
-    def __len__(self):
-        return len(self.y)
-
-
 def set_all_seeds(SEED):
     # REPRODUCIBILITY
     torch.manual_seed(SEED)
@@ -74,7 +50,6 @@ def set_all_seeds(SEED):
 
 
 def load_data(SEED, use_subset, batch_size, imratio):
-    # TODO: stratify labels in unbalanced dataset
     (train_data, train_label), (test_data, test_label) = CIFAR10()
     (train_images, train_labels) = ImbalanceGenerator(train_data, train_label, imratio=imratio, shuffle=True,
                                                       random_seed=SEED)
