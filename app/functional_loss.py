a2b62a75f6f2ff4ee40b7a98d522de2cd094cec7
rustky <kylerustxc@gmail.com>
2021-11-17 09:17:16 -0700

    update functional losses

--- app/functional_loss.py
+++ app/functional_loss.py
@@ -4,7 +4,8 @@ def square_hinge(predictions, labels, margin=1):
     I_pos = torch.where(labels == 1)[0]
     I_neg = torch.where(labels == 0)[0]
     torch.autograd.set_detect_anomaly(True)
-    a_coeff, b_coeff, c_coeff, running_loss = 0,0,0,0
+    a_coeff, b_coeff, c_coeff = torch.Tensor([0]),torch.Tensor([0]),torch.Tensor([0])
+    running_loss = torch.Tensor([0])
     labels_length = len(labels)
     augmented_predictions = torch.zeros(labels_length)
     for i in range(0, labels_length):
@@ -16,13 +17,13 @@ def square_hinge(predictions, labels, margin=1):
     for j in range(0, labels_length):
         augmented_indicies = augmented_predictions_sorted[j]
         predicted_value = predictions[augmented_indicies]
-        if labels[augmented_indicies] == 1: 
+        if labels[augmented_indicies] == 1:
             z_coeff = margin - predicted_value
             a_coeff += 1
             b_coeff += 2*z_coeff
             c_coeff += z_coeff**2
         else:
-            running_loss += a_coeff*(predicted_value**2) + b_coeff*(predicted_value) + c_coeff
+            running_loss += a_coeff*(predicted_value**2) + b_coeff*predicted_value + c_coeff
     return running_loss/(len(I_pos)*len(I_neg))
 
 def square(predictions,labels, margin=1):
--- app/functional_loss_test.py
+++ app/functional_loss_test.py
@@ -1,11 +1,7 @@
 import torch
 
 def square_hinge_test(predictions, labels, margin=1):
-    I_pos = torch.where(labels == 1)[0]
-    I_neg = torch.where(labels == 0)[0]
     torch.autograd.set_detect_anomaly(True)
-    a_coeff, b_coeff, c_coeff = torch.Tensor([1]),torch.Tensor([0]),torch.Tensor([0])
-    running_loss = torch.Tensor([0])
     labels_length = len(labels)
     augmented_predictions = torch.zeros(labels_length)
     for i in range(0, labels_length):
@@ -16,12 +12,14 @@ def square_hinge_test(predictions, labels, margin=1):
     augmented_predictions_sorted = torch.argsort(augmented_predictions)
     predicted_value = predictions[augmented_predictions_sorted]
     labels_sorted = labels[augmented_predictions_sorted]
+    I_pos = torch.where(labels_sorted == 1)[0]
+    I_neg = torch.where(labels_sorted == 0)[0]
     z_coeff = margin - predicted_value
     a_coeff = torch.cumsum(labels_sorted, dim = 0)
     b_coeff = torch.cumsum(2*z_coeff*labels_sorted, dim = 0)
     c_coeff = torch.cumsum((z_coeff**2)*labels_sorted, dim = 0)
     loss_values = a_coeff*(predicted_value**2) + b_coeff*predicted_value + c_coeff
-    return sum(loss_values[I_pos])/(len(I_pos)*len(I_neg))
+    return sum(loss_values[I_neg])/(len(I_pos)*len(I_neg))
 
 def square_test(predictions,labels, margin=1):
     a_coeff, b_coeff, c_coeff, running_loss = 0,0,0,0
@@ -34,5 +32,5 @@ def square_test(predictions,labels, margin=1):
     a_coeff = torch.sum(sorted_labels, dim = 0)
     b_coeff = torch.sum(2*z_coeff*sorted_labels, dim = 0)
     c_coeff = torch.sum((z_coeff**2)*sorted_labels, dim = 0)
-    loss_values = a_coeff*(predictions**2) + b_coeff*predictions + c_coeff
+    loss_values = a_coeff*(predicted_value**2) + b_coeff*predicted_value + c_coeff
     return sum(loss_values[I_neg])/(len(I_pos)*len(I_neg))
