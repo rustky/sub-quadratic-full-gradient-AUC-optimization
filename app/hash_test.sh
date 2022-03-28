b739181a72fc78d8f112145d272af427690f1b7a
Kyle Rust <krr387@wind.nauhpc>
2022-03-28 14:28:19 -0700

    add ntasks line

--- app/hash_test.sh
+++ app/hash_test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=hash_test                        # the name of your job
-#SBATCH --output=/projects/genomic-ml/sub-quadratic-full-gradient-AUC-optimization/app/results/test/error.txt
+#SBATCH --output=/projects/genomic-ml/sub-quadratic-full-gradient-AUC-optimization/app/results/test/error_10.txt
 #SBATCH --time=120:00                            # 20 min, shorter time, quicker start, max run time
 #SBATCH --mem=32000                              # 2GB of memory
 #SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
 #SBATCH --mail-type=FAIL
 
 
 # Run your application: precede the application command with 'srun'
 # A couple example applications...
 
-python3 temp_driver.py 10 0.1 square_hinge_test .0001 ./results/test CAT_VS_DOG ResNet20 SGD 5
+python3 temp_driver.py 10 0.1 square_hinge_test .0001 ./results/test CIFAR10 ResNet20 SGD 10
