a7c38c93f6d43a1929bafe315915210517afe74e
rustky <kylerustxc@gmail.com>
2022-12-07 12:51:19 -0700

    remove extra for loop to improve overhead

--- app/functional_loss_test.py
+++ app/functional_loss_test.py
@@ -3,23 +3,20 @@ import torch
 def square_hinge_test(predictions, labels, margin=1):
     torch.autograd.set_detect_anomaly(True)
     labels_length = len(labels)
-    augmented_predictions = torch.zeros(labels_length)
-    for i in range(0, labels_length):
-        if labels[i] == 0:
-            augmented_predictions[i] = predictions[i] + margin
-        else:
-            augmented_predictions[i] = predictions[i]
-    augmented_predictions_sorted = torch.argsort(augmented_predictions)
-    predicted_value = predictions[augmented_predictions_sorted]
-    labels_sorted = labels[augmented_predictions_sorted]
+    augmented_predictions = torch.where(labels == 1, predictions,
+                                        predictions + margin)
+    augmented_indices_sorted = torch.argsort(augmented_predictions)
+    predicted_value = predictions[augmented_indices_sorted]
+    labels_sorted = labels[augmented_indices_sorted]
     I_pos = torch.where(labels_sorted == 1)[0]
     I_neg = torch.where(labels_sorted == 0)[0]
+    N = len(I_pos)*len(I_neg)
     z_coeff = margin - predicted_value
-    a_coeff = torch.cumsum(labels_sorted, dim = 0)
-    b_coeff = torch.cumsum(2*z_coeff*labels_sorted, dim = 0)
-    c_coeff = torch.cumsum((z_coeff**2)*labels_sorted, dim = 0)
+    a_coeff = torch.cumsum((labels_sorted)/N, dim = 0)
+    b_coeff = torch.cumsum((2*z_coeff*labels_sorted)/N, dim = 0)
+    c_coeff = torch.cumsum(((z_coeff**2)*labels_sorted)/N, dim = 0)
     loss_values = a_coeff*(predicted_value**2) + b_coeff*predicted_value + c_coeff
-    return sum(loss_values[I_neg])/(len(I_pos)*len(I_neg))
+    return sum(loss_values[I_neg])
 
 def square_test(predictions,labels, margin=1):
     I_pos = torch.where(labels == 1)[0]
